17e6afeae298498108abbe59b6229f766a7a7f1e
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-05-27 10:59:57 +0100

    Sql tables updated

---
+++ FireFightLibraryUnitTests/DBTests.cs
@@ -0,0 +1,55 @@
+﻿using FireFightRedux;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class DBTests
+    {
+        [TestMethod]
+        public void DataTableConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.DataTableConnection.Open();
+                dBFunctions.DataTableConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ArmouryConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.ArmouryConnection.Open();
+                dBFunctions.ArmouryConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CharacterConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.CharactersConnection.Open();
+                dBFunctions.CharactersConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
---
+++ FirefightUnitTests/DBTests.cs
@@ -0,0 +1,53 @@
+using FireFight.Functions;
+
+namespace FirefightUnitTests
+{
+    [TestClass]
+    public class DBTests
+    {
+        [TestMethod]
+        public void DataTableConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.DataTableConnection.Open();
+                dBFunctions.DataTableConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ArmouryConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.ArmouryConnection.Open();
+                dBFunctions.ArmouryConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void CharacterConnectionTest()
+        {
+            DBFunctions dBFunctions = new DBFunctions();
+            try
+            {
+                dBFunctions.CharactersConnection.Open();
+                dBFunctions.CharactersConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}
