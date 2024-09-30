9a478b6e973c1d4155d4eca503589819e02a013e
Jake Trans <Jake_Damner@yahoo.co.uk>
2024-09-30 11:44:24 +0100

    Impulse class first version

---
+++ FireFightLibrary/Classes/Impulses.cs
@@ -0,0 +1,52 @@
+﻿using FireFight.CharacterObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireFightLibrary.Classes
+{
+    public class Impulse
+    {
+        public Character Character { get; set; }
+        public Action Action { get; set; }
+
+        public Impulse(Character character, Action action)
+        {
+            if (character == null)
+            {
+                throw new ArgumentNullException(nameof(character));
+            }
+
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            Character = character;
+            Action = action;
+        }
+    }
+
+    public class Impulses
+    {
+        public List<Impulse> ImpulseList1 { get; set; }
+        public List<Impulse> ImpulseList2 { get; set; }
+        public List<Impulse> ImpulseList3 { get; set; }
+        public List<Impulse> ImpulseList4 { get; set; }
+
+        public Impulses()
+        {
+            ImpulseList1 = new List<Impulse>();
+            ImpulseList2 = new List<Impulse>();
+            ImpulseList3 = new List<Impulse>();
+            ImpulseList4 = new List<Impulse>();
+        }
+
+        public static void SortImpulseListByINTSkill(List<Impulse> impulseList)
+        {
+            impulseList.Sort((x, y) => x.Character.INTSkillFactor.CompareTo(y.Character.INTSkillFactor));
+        }
+    }
+}
